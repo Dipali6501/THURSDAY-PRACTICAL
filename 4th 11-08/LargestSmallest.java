68b5ccc9d9c4283c90ddbdf776d6792bb905987c
Dipali6501 <108930575+Dipali6501@users.noreply.github.com>
2022-08-20 23:22:04 +0530

    Add files via upload

---
+++ 4th 11-08/LargestSmallest.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+
+
+public class LargestSmallest
+
+
+	{
+	
+
+	public static void main(String args[])
+   
+     		{
+		
+		int arr[] = { 50 , 12 , 1 , 97 , 34 , 72 };
+
+			        int largest = arr[0]; 
+	
+			int smallest= arr[0];
+
+
+
+				for(int i=0; i<arr.length; i++) 
+
+
+				{
+	
+				 if(smallest > arr[i])
+	
+					{
+     
+         					
+	smallest = arr[i];
+	
+			
+		}
+           		
+		if(largest < arr[i])
+
+					
+	{
+              		
+				largest = arr[i];
+						}
+				}
+
+       				System.out.println("Smallest Element in Array is : "+smallest);
+  
+
+    	System.out.println("Largest Element in Array is : "+largest);
+   		
+	}
+	
+}
